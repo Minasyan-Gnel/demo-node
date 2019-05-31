601fa6a5e845d429ceb818b55c78bf64ff47e43f
gnel <gnel.minasyan@betconstruct.com>
2019-05-31 11:59:13 +0400

    rename folder

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require('body-parser');
 const serverConst = require("./constants/server");
-const usersRequests = require("./requests/usersRequests");
+const usersRoutes = require("./routes/usersRoutes");
 const mongoDB = require("./mongoDB");
 const app = express();
 
@@ -13,7 +13,7 @@ app.use(cors({
     origin: "http://localhost:3001"
 }));
 
-app.use(usersRequests);
+app.use(usersRoutes);
 
 app.listen(serverConst.port, () => {
     console.log(`Server listen in port ${serverConst.port}`)
